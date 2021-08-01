65bfb97e14795fad6b1055cbb3e93bb9ad2ec502
gyanendra basnet <bgyanendra93@gmail.com>
2021-08-01 18:55:52 -0500

    initial commit

---
+++ SeleniumTest/ConfigFileTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    [TestFixture]
+    public class ConfigFileTest : HotelApp_BusinessFunction
+    {
+        private new IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver(@"D:\Drivers\chromedriver_win32");
+            baseURL = "http://adactin.com/";
+            verificationErrors = new StringBuilder();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        [Test]
+        public void MyConfigFileTest()
+        {
+            driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["sAppURL"]);
+
+            HA_BF_Login(driver, "adactin123", "adactin123");
+            new SelectElement(driver.FindElement(By.Id(ConfigurationManager.AppSettings["Lst_SearchHotel_Location"]))).SelectByText("Sydney");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Btn_SearchHotel_Search"])).Click();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Rad_SelectHotel_RadioButton_1"])).Click();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Btn_SelectHotel_Continue"])).Click();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_FirstName"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_FirstName"])).SendKeys("test");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_LastName"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_LastName"])).SendKeys("test");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_Address"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_Address"])).SendKeys("test");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_CCNumber"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_CCNumber"])).SendKeys("1212121212121212");
+            new SelectElement(driver.FindElement(By.Id(ConfigurationManager.AppSettings["Lst_BookingHotel_CCType"]))).SelectByText("American Express");
+            new SelectElement(driver.FindElement(By.Id(ConfigurationManager.AppSettings["Lst_BookingHotel_CCExpMonth"]))).SelectByText("March");
+            new SelectElement(driver.FindElement(By.Id(ConfigurationManager.AppSettings["Lst_BookingHotel_CCExpYear"]))).SelectByText("2015");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_CCCvvNumber"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_BookingHotel_CCCvvNumber"])).SendKeys("111");
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Btn_BookingHotel_BookNow"])).Click();
+            driver.FindElement(By.LinkText(ConfigurationManager.AppSettings["Lnk_BookingHotel_Logout"])).Click();
+            driver.FindElement(By.LinkText(ConfigurationManager.AppSettings["Lnk_Logout_ClickHeretoLoginAgain"])).Click();
+            IWebElement txtbox_username = driver.FindElement(By.Id("username"));
+
+
+        }
+    }
+}
---
+++ SeleniumTest/HotelApp_BusinessFunction.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SeleniumTest
+{
+    public class HotelApp_BusinessFunction
+    {
+        public static IWebDriver driver;
+
+        public object ConfigurationManager { get; private set; }
+
+        public void HA_BF_Login(IWebDriver driver, string sUserName, string sPassword)
+        {
+            //Provide Username
+            driver.FindElement(By.XPath(ConfigurationManager.AppSettings["Txt_Login_Username"])).Clear();
+            driver.FindElement(By.XPath(ConfigurationManager.AppSettings["Txt_Login_Username"])).SendKeys(sUsername);
+            //Provide Password
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_Login_Password"])).Clear();
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Txt_Login_Password"])).SendKeys(sPassword);
+
+            //Click on Login button
+            driver.FindElement(By.Id(ConfigurationManager.AppSettings["Btn_Login_Login"])).Click();
+
+
+        }
+        //Function to dynamically wait for element presence
+        public void HA_GF_WaitForElementPresent(IWebDriver driver, By by, int iTimeOut)
+        {
+            try
+            {
+                int iTotal = 0;
+                int iSleepTime = 5000;
+                while (iTotal < iTimeOut)
+                {
+                    IReadOnlyCollection<IWebElement> iWebElements = driver.FindElements(by);
+                    if (iWebElements.Count > 0)
+                        return;
+                    else
+                    {
+
+                        Thread.Sleep(iSleepTime);
+                        iTotal = iTotal + iSleepTime;
+                        Console.Write(String.Concat("Waited for " + iTotal + " milliseconds " + by));
+
+                    }
+                }
+            }
+            catch (ElementNotVisibleException)
+            {
+                return;
+            }
+        }
+    }
+}
